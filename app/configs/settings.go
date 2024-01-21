1d6516c02cd90b76b4b4d22d84f1b0c6f78532b3
takaak-endo <t.endo@branu.jp>
2024-01-21 18:28:24 +0900

    テスト用のDBと本番用のDBの環境変数変更に対応できるように

--- app/configs/settings.go
+++ app/configs/settings.go
@@ -8,6 +8,16 @@ import (
 
 type Settings struct {
 	JwtSecret string `env:"JwtSecret,required"`
+
+	DbHostName string `env:"DbHostName" envDefault:"db:3306"`
+	DbName     string `env:"DbName" envDefault:"ec_site"`
+	DbUserName string `env:"DbUserName" envDefault:"root"`
+	DbUserPass string `env:"DbUserPass" envDefault:"root"`
+
+	TestDbHostName string `env:"TestDbHostName" envDefault:"db:3306"`
+	TestDbName     string `env:"TestDbName" envDefault:"ec_site_test"`
+	TestDbUserName string `env:"TestDbUserName" envDefault:"root"`
+	TestDbUserPass string `env:"TestDbUserPass" envDefault:"root"`
 }
 
 var cfg Settings = Settings{}
