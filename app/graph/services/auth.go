2447c3d2964143154325fc45de024bc72926d11f
takaak-endo <t.endo@branu.jp>
2024-01-21 18:56:23 +0900

    auth service classの内容を修正

--- app/graph/services/auth.go
+++ app/graph/services/auth.go
@@ -3,52 +3,42 @@ package services
 import (
 	"context"
 	"errors"
-	"time"
+	"strconv"
 
-	"github.com/endot1231/ec-backend/configs"
+	"github.com/endot1231/ec-backend/clock"
 	"github.com/endot1231/ec-backend/ent"
 	"github.com/endot1231/ec-backend/ent/users"
-	"github.com/golang-jwt/jwt/v5"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/endot1231/ec-backend/secrets"
 )
 
 type authService struct {
-	exec ent.Client
+	exec    ent.Client
+	Clocker clock.Clocker
 }
 
 func (u *authService) Login(ctx context.Context, role string, email string, password string) (string, error) {
+
 	user, err := u.exec.Users.Query().Where(
-		users.Email(email),
-	).First(ctx)
+		users.EmailEQ(email),
+	).Only(ctx)
+
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
 
 	if user == nil {
 		return "", errors.New("failed login")
 	}
 
+	err = secrets.CompareValue(user.Password, password)
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
+
+	jwt, err := secrets.JwtGenerate(role, strconv.Itoa(user.ID), u.Clocker.Now())
 	if err != nil {
 		return "", err
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	if err != nil {
-		return "", errors.New("failed login")
-	}
-
-	claims := jwt.MapClaims{
-		"ExpiresAt": jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
-		"IssuedAt":  jwt.NewNumericDate(time.Now()),
-		"NotBefore": jwt.NewNumericDate(time.Now()),
-		"Issuer":    "endot1231",
-		"Role":      "user",
-		"ID":        user.ID,
-	}
-
-	mySigningKey := []byte(configs.Get().JwtSecret)
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString(mySigningKey)
-	if err != nil {
-		return "", err
-	}
-
-	return ss, nil
+	return jwt, nil
 }
